fa92c6085141ee47d3caf9a4563f8039fc107aac
Jeet Narayan <jeet-5870@users.noreply.github.com>
2025-09-16 17:51:34 +0530

    url updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://groundwater-backend.onrender.com', // ✅ Backend running on port 3000
+  baseURL: 'https://beyond-null-and-void.onrender.com', // ✅ Backend running on port 3000
 });
 
 export default API;
