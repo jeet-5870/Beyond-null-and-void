1addbc7f1f736df09fa464d2fc3faf73c905e65f
Jeet Narayan <jeet-5870@users.noreply.github.com>
2025-09-17 09:15:00 +0530

    tailwind fixing

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../components/card.jsx",
-    "../components/pollutionChart.jsx",
-    "../components/resultTable.jsx",
-    "../components/safetyBadge.jsx",
-    "../components/uploadForm.jsx",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
