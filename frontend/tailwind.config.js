3d3704ef5a2d1cdaf7db866fce7a4566a974dd83
Jeet Narayan <jeet-5870@users.noreply.github.com>
2025-09-17 08:40:42 +0530

    tailwind implement

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../components/card.jsx",
+    "../components/pollutionChart.jsx",
+    "../components/resultTable.jsx",
+    "../components/safetyBadge.jsx",
+    "../components/uploadForm.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
