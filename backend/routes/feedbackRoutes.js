a5c80d351a65a9a7cd9bfc1765b9abd9f2058dc0
Jeet Narayan <jeet-5870@users.noreply.github.com>
2025-09-20 22:01:17 +0530

    right file name import

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { submitFeedback, getFeedback } from '../controllers/feedbackController.js';
+import { submitFeedback, getFeedback } from '../controllers/feedbackControllers.js';
 
 const router = express.Router();
 
